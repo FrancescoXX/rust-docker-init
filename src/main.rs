62ed5676bdea10a00ec3682ba116b010f3486186
FrancescoXX <francescociullaroma@gmail.com>
2024-08-30 14:14:29 +0200

    init

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+async fn greet() -> impl Responder {
+    HttpResponse::Ok().body("Hello, world!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+    })
+    .bind("0.0.0.0:8080")?
+    .run()
+    .await
+}
+
